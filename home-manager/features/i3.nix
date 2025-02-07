d6ea1bccf84cee5562ea7f0908e01907fb0c3f31
emilis118 <uleu2em@gmail.com>
2025-02-07 16:21:43 +0100

    i3 path fix

--- home-manager/features/i3.nix
+++ home-manager/features/i3.nix
@@ -4,7 +4,8 @@
 
     # home.directory."$HOME/.config/i3".source = ./../i3;
     xsession.windowManager.i3.enable = true;
-    xsession.windowManager.i3.extraConfig = "include $HOME/dotfiles/i3/config";
+    # xsession.windowManager.i3.extraConfig = "include $HOME/dotfiles/i3/config";
+    xsession.windowManager.i3.configFile = "$HOME/dotfiles/i3/config";
 
     home.packages = with pkgs; [
             feh
