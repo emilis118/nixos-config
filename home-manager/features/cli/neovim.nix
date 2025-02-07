d4672fa0bab36a7f96b9a7cf91a4c1a41d6ed343
emilis118 <uleu2em@gmail.com>
2025-02-07 23:38:49 +0100

    neovim conf

--- home-manager/features/cli/neovim.nix
+++ home-manager/features/cli/neovim.nix
@@ -3,14 +3,50 @@
 {
   programs.neovim = {
     enable = true;
-    # Path to your custom Neovim configuration
-    extraLuaConfig = ''
-      source /etc/nixos/dotfiles/nvim/init.lua
-    '';
+    # alias
+    viAlias = true;
+    vimAlias = true;
+    vimdiffAlias = true;
+    defaultEditor = true;
+    # extraLuaConfig = ''
+    #   -- ${builtins.readFile ./something.lua}
+    # '';
+    
+    plugins = with pkgs.vimPlugins; [
+        # regular:
+        # config-name
+        # or
+        # with config:
+        # {plugin = config-name; config = ""; type = "lua";}
+        {
+            plugin = catppucin-nvim;
+            type = "lua";
+            config = "colorscheme catppucin";
+        }
+        # harpoon requirements:
+        plenary-nvim
+        telescope-nvim
+        {
+            plugin = harpoon2; 
+            type = "lua";
+            config = builtins.readFile ./../../../dotfiles/nvim/after/plugin/harpoon.lua;
+        }
+
+        (nvim-treesitter.withPlugins (p: [
+            p.tree-sitter.nix
+            p.tree-sitter.vim
+            p.tree-sitter.bash
+            p.tree-sitter.lua
+            p.tree-sitter.python
+            p.tree-sitter.json
+# add others later
+        ]))
+    ];
   };
 
   # packages to have
   home.packages = with pkgs; [
+    xclip
     ripgrep
     fd
     gcc
