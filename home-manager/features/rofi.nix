db53bdfc06dcf80b653d009ed51662da7150038a
emilis118 <uleu2em@gmail.com>
2025-02-13 14:08:04 +0100

    rofi

--- home-manager/features/rofi.nix
+++ home-manager/features/rofi.nix
@@ -1,31 +1,23 @@
 { pkgs, ... }:
 
-    let
-    rofiPowerMenu = "${pkgs.rofi-power-menu}/bin/rofi-power-menu";
-    in {
+{
     programs.rofi = {
         enable = true;
         plugins = with pkgs; [
             rofi-calc  # calculator
-            rofi-bluetooth  # bluetooth manager
             rofi-top  # load like htop
-            rofi-screenshot  # screenshot and screenrecord
-            rofi-power-menu  # power menu
-            rofi-pulse-select  # select audio output
-            todofi-sh  # todo list
         ];
         extraConfig = {
-            modi = "window,run,ssh,top,power";
+            modi = "window,run,ssh,top,power,bluetooth:rofi-bluetooth,ss:rofi-screenshot,audio:rofi-pulse-select";
             };
         };
 
-    home.file.".config/rofi/scripts/rofi-power-menu.sh".text = ''
-        #!/bin/sh
-        ${rofiPowerMenu}
-    '';
-
-    home.packages = [ pkgs.rofi-power-menu ];
-
-    home.sessionVariables.ROFI_POWER_MENU = rofiPowerMenu;
+    home.packages = with pkgs; [ 
+    rofi-bluetooth
+    rofi-screenshot
+    rofi-power-menu 
+    rofi-pulse-select
+    todofi-sh
+    ];
 
 }
