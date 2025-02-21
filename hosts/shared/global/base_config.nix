15a34c9f54b6b7ec62113ccd1118c3ac9e8066ac
emilis118 <uleu2em@gmail.com>
2025-02-21 17:39:35 +0100

    unfree fix

--- hosts/shared/global/base_config.nix
+++ hosts/shared/global/base_config.nix
@@ -9,7 +9,7 @@
 }: {
   # enable flakes
   nix.settings.experimental-features = ["nix-command" "flakes"];
-  nix.nixPath = ["nixpkgs=${inputs.nixpkgs}"];
+  # nix.nixPath = ["nixpkgs=${inputs.nixpkgs}"];
   # Enable networking
   networking.networkmanager.enable = true;
   # Enable the X11 windowing system.
