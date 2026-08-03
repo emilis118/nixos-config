1f794913cdf877f3dbd4bd7d959f5d0eb567fd93
emilis118 <uleu2em@gmail.com>
2026-08-03 23:56:33 +0200

    install on ssd

--- hosts/amd-desktop/hardware-configuration.nix
+++ hosts/amd-desktop/hardware-configuration.nix
@@ -8,23 +8,25 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/782b92e3-5d2e-46ee-8e2f-9d8b61121885";
+    { device = "/dev/disk/by-uuid/cd0c1315-dcfd-4fae-954f-3478987ad52b";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/2E01-198C";
+    { device = "/dev/disk/by-uuid/86C9-2C78";
       fsType = "vfat";
       options = [ "fmask=0077" "dmask=0077" ];
     };
 
-  swapDevices = [ ];
+  swapDevices =
+    [ { device = "/dev/disk/by-uuid/eedd1926-8516-44fd-a0da-133bb9c6fcd4"; }
+    ];
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
